792a1f358e1139f38af1c6142dfda85a366b49f3
maream khaled ahmed <mareamkmousbah@gmail.com>
2025-03-13 00:32:02 +0200

    Refactor HomeItem to use Bloc for state management and simplify widget structure

--- lib/home_item.dart
+++ lib/home_item.dart
@@ -8,83 +8,56 @@ class HomeItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //  int counter = 0;
-
-    // print("ppppppp");
     return BlocProvider(
+      // Create a new instance of AzkarCubit for each HomeItem
       create: (context) => AzkarCubit(),
-      child: Column(
-        children: [
-          Image.asset("assets/quran.png"),
-          Text(
-            text,
-            style: TextStyle(fontSize: 35, fontWeight: FontWeight.w700),
-          ),
-          BlocBuilder<AzkarCubit, AzkarState>(
-            builder: (context, state) {
-              return Text(
-                '${state.counter}',
-                style: TextStyle(fontSize: 35, fontWeight: FontWeight.w700),
-              );
-            },
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+      child: Builder(
+        builder: (context) {
+          return Column(
             children: [
+              Image.asset("assets/quran.png"),
+              Text(
+                text,
+                style: TextStyle(fontSize: 35, fontWeight: FontWeight.w700),
+              ),
               BlocBuilder<AzkarCubit, AzkarState>(
                 builder: (context, state) {
-                  return IconButton(
+                  return Text(
+                    '${state.counter}',
+                    style: TextStyle(fontSize: 35, fontWeight: FontWeight.w700),
+                  );
+                },
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
                     iconSize: 50,
                     onPressed: () {
                       context.read<AzkarCubit>().resetCounter();
-                      // setState(() {
-                      //   counter = 0;
-                      // });
                     },
                     icon: Icon(Icons.refresh),
-                  );
-                },
-              ),
-              BlocBuilder<AzkarCubit, AzkarState>(
-                builder: (context, state) {
-                  return IconButton(
+                  ),
+                  IconButton(
                     iconSize: 50,
                     onPressed: () {
-                      // print(state.counter);
-
                       context.read<AzkarCubit>().addCounter();
-                      // print(state.counter);
-
-                      // setState(() {
-                      //   counter++;
-                      // });
                     },
                     icon: Icon(Icons.add),
-                  );
-                },
-              ),
-              BlocBuilder<AzkarCubit, AzkarState>(
-                builder: (context, state) {
-                  return IconButton(
+                  ),
+                  IconButton(
                     iconSize: 50,
                     onPressed: () {
                       context.read<AzkarCubit>().speak(text);
-                      // setState(() {
-                      //   speak(widget.text);
-                      // });
                     },
                     icon: Icon(Icons.hearing),
-                  );
-                },
+                  ),
+                ],
               ),
             ],
-          )
-        ],
+          );
+        },
       ),
     );
   }
 }
-
-//set state 
-
-// 0 1 2 3 
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,7 @@
+import 'package:azkar/cubit/cubit/azkar_cubit.dart';
 import 'package:azkar/home_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
